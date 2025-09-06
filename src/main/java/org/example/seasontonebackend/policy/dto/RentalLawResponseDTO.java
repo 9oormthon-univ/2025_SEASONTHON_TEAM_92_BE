a26c6a98fd8e30cfdcca0eb9faccfe161cbb8497
imes <a01083302623@gmail.com>
2025-09-07 05:21:20 +0900

    정책 관련 구현

--- src/main/java/org/example/seasontonebackend/policy/dto/RentalLawResponseDTO.java
+++ src/main/java/org/example/seasontonebackend/policy/dto/RentalLawResponseDTO.java
@@ -32,5 +32,9 @@ public class RentalLawResponseDTO {
         public static <T> ApiResponse<T> success(T data, String message) {
             return new ApiResponse<>(true, data, message);
         }
+
+        public static <T> ApiResponse<T> error(String message) {
+            return new ApiResponse<>(false, null, message);
+        }
     }
 }
