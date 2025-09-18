ce5cc1986fbea9e8dbe5c788052e96e308af8ef8
rkddngpr <rkddngpr@unist.ac.kr>
2025-09-18 22:59:55 +0900

    Security: Remove hardcoded mail credentials and use environment variables

--- src/main/java/org/example/seasontonebackend/SeasonToneBackendApplication.java
+++ src/main/java/org/example/seasontonebackend/SeasonToneBackendApplication.java
@@ -2,6 +2,7 @@ package org.example.seasontonebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
 
 @SpringBootApplication
 public class SeasonToneBackendApplication {
